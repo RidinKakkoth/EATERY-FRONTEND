d0137b5ec332acfcd4d649876654bfd144bdb39b
Ridin Kakkoth <k.ridin@ymail.com>
2024-05-27 13:22:48 +0530

    navbar component structure done

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,22 +1,29 @@
+import { useState } from 'react'
 import images from '../../assets/images'
 import './Navbar.css'
 const Navbar = () => {
+
+
+    const[menu,setMenu]=useState("home")
+
+
+
   return (
-    <div className=' py-5 px-0 flex justify-between items-center'>
-        <img src={images.logo} alt="" className="logo" />
-        <ul className="navbar-menu">
-            <li className='bg-yellow-500 text-white'>home</li>
-            <li>menu</li>
-            <li>mobile-app</li>
-            <li>contact-us</li>
+    <div className='navbar py-5 px-0 flex justify-between items-center'>
+        <img src={images.logo} alt="logo" className="w-[150px]" />
+        <ul className="flex list-none gap-5 text-bluegrey text-[18px]">
+            <li onClick={()=>{setMenu("home")}} className={menu=="home"?"active":""}>home</li>
+            <li onClick={()=>{setMenu("menu")}} className={menu=="menu"?"active":""}>menu</li>
+            <li onClick={()=>{setMenu("mobile-app")}} className={menu=="mobile-app"?"active":""}>mobile-app</li>
+            <li onClick={()=>{setMenu("contact-us")}} className={menu=="contact-us"?"active":""}>contact-us</li>
         </ul>
-        <div className="navbar-right">
+        <div className="flex items-center gap-10">
             <img src={images.search} alt="" className="search" />
-            <div className="navbar-search-icon">
+            <div className=" relative ">
                 <img src={images.basket} alt="" className="basket" />
-                <div className="dot"></div>
+                <div className=" absolute min-w-[10px] min-h-[10px] bg-eatery border rounded-full top-[-8px] right-[-8px] "></div>
             </div>
-            <button>signin</button>
+            <button className='text-[16px] text-bluegrey border border-solid border-eatery rounded-full px-[30px] py-[10px] cursor-pointer hover:bg-[#fff4f2]'>signin</button>
         </div>
     </div>
   )
